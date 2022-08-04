eff63b67bb9c9e5a7918713357acad320544f074
CheeYuan <20047102@myrp.edu.sg>
2022-08-04 11:21:55 +0800

    Added Dialog Boxes for Update movie, Delete movie, and Discard changes with respective toast messages if successful

--- app/src/main/java/sg/edu/rp/c346/id20047102/mymovies/updateMovies.java
+++ app/src/main/java/sg/edu/rp/c346/id20047102/mymovies/updateMovies.java
@@ -1,13 +1,16 @@
 package sg.edu.rp.c346.id20047102.mymovies;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class updateMovies extends AppCompatActivity {
 
@@ -49,32 +52,91 @@ public class updateMovies extends AppCompatActivity {
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create the Dialog Builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(updateMovies.this);
+
+                // Set the dialog details
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to save the changes");
+                myBuilder.setCancelable(false);
 
                 DBHelper dbh = new DBHelper(updateMovies.this);
-                data.setmTitle(etTitle.getText().toString());
-                data.setmGenre(etGenre.getText().toString());
-                data.setmYear(Integer.parseInt(etYear.getText().toString()));
-                data.setmRatings(spinRating.getSelectedItem().toString());
 
-                dbh.updateMovie(data);
-                dbh.close();
-                finish();
+                myBuilder.setNegativeButton("UPDATE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        data.setmTitle(etTitle.getText().toString());
+                        data.setmGenre(etGenre.getText().toString());
+                        data.setmYear(Integer.parseInt(etYear.getText().toString()));
+                        data.setmRatings(spinRating.getSelectedItem().toString());
+                        Toast.makeText(updateMovies.this, "Movie updated successfully",
+                                Toast.LENGTH_SHORT).show();
+                        dbh.updateMovie(data);
+                        dbh.close();
+                        finish();
+                    }
+                });
+                myBuilder.setPositiveButton("CANCEL", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
             }
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create the Dialog Builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(updateMovies.this);
+
+                // Set the dialog details
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to delete the movie " + etTitle.getText().toString());
+                myBuilder.setCancelable(false);
+
                 DBHelper dbh = new DBHelper(updateMovies.this);
-                dbh.deleteMovie(data.getmId());
-                finish();
+
+                myBuilder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dbh.deleteMovie(data.getmId());
+                        Toast.makeText(updateMovies.this, "The movie " + etTitle.getText().toString() + " has been deleted successfully",
+                                Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+
+                myBuilder.setPositiveButton("CANCEL", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
             }
         });
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                // Create the Dialog Builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(updateMovies.this);
+
+                // Set the dialog details
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to discard the changes");
+                myBuilder.setCancelable(false);
+
+                myBuilder.setNegativeButton("DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(updateMovies.this, "Changes have been discarded",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
+                myBuilder.setPositiveButton("DO NOT DISCARD", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
             }
         });
 
